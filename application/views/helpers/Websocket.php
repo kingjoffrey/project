5c595c6ca6e381206609690c4c80b1ea2a0b3125
Bartosz Krzeszewski <bartek@core-t.pl>
2014-02-03 23:50:51 +0100

    m

--- application/views/helpers/Websocket.php
+++ application/views/helpers/Websocket.php
@@ -1,25 +1,17 @@
 <?php
 
-class Zend_View_Helper_Websocket extends Zend_View_Helper_Abstract {
-
-    public function websocket() {
-
-        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/swfobject.js');
-        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/web_socket.js');
-
-        $url = Zend_Registry::get('config')->websockets->aSchema . '://' . Zend_Registry::get('config')->websockets->aHost . ':' . Zend_Registry::get('config')->websockets->aPort;
+class Zend_View_Helper_Websocket extends Zend_View_Helper_Abstract
+{
+    public function websocket()
+    {
+        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/flash-bridge/swfobject.js');
+        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/flash-bridge/web_socket.js');
 
         $script = '
-        WEB_SOCKET_SWF_LOCATION = "WebSocketMain.swf";
+        WEB_SOCKET_SWF_LOCATION = "/js/flash-bridge/WebSocketMain.swf";
         WEB_SOCKET_DEBUG = true;
+        var wsURL = "' . Zend_Registry::get('config')->websockets->aSchema . '://' . Zend_Registry::get('config')->websockets->aHost . ':' . Zend_Registry::get('config')->websockets->aPort . '"';
 
-        var wsURL = "' . $url . '";
-        var wsClosed = true;
-        var ws;
-        var loading = true;
-';
         $this->view->headScript()->appendScript($script);
     }
-
 }
-
--- public/js/editor/init.js
+++ public/js/editor/init.js
@@ -5,3 +5,31 @@ $(document).ready(function () {
     Editor.init()
     Gui.init()
 })
+
+var Aaa = function () {
+    this.aaa = 'To jest a'
+}
+
+Aaa.prototype.do = function () {
+    this.a = 'zaqwsx'
+    console.log('I do things')
+}
+
+var Bbb = function () {
+    this.bbb = 'To jest b'
+    this.do = function () {
+        console.log('x')
+        Aaa.prototype.do.apply(this)
+        console.log(this.a)
+    }
+    this.done = function () {
+        console.log('Something is done')
+    }
+}
+
+Bbb.prototype = new Aaa()
+//Bbb.prototype.constructor = Bbb
+
+bb = new Bbb()
+
+bb.do()
