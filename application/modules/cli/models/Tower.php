e1b3055157a913d29a17726498ee988793b58927
Bartosz Krzeszewski <bartek@core-t.pl>
2014-10-12 05:40:13 +0200

    m

---
+++ application/modules/cli/models/Tower.php
@@ -0,0 +1,32 @@
+<?php
+
+class Cli_Model_DisbandArmy
+{
+
+    public function __construct($armyId, $user, $db, $gameHandler)
+    {
+        if (empty($armyId)) {
+            $gameHandler->sendError($user, 'Brak "armyId"!');
+            return;
+        }
+
+        $mArmy = new Application_Model_Army($user->parameters['gameId'], $db);
+        $destroyArmyResponse = $mArmy->destroyArmy($armyId, $user->parameters['playerId']);
+
+        if (!$destroyArmyResponse) {
+            $gameHandler->sendError($user, 'Nie mogę usunąć armii!');
+            return;
+        }
+
+        $playersInGameColors = Zend_Registry::get('playersInGameColors');
+
+        $token = array(
+            'type' => 'disband',
+            'armyId' => $armyId,
+            'color' => $playersInGameColors[$user->parameters['playerId']]
+        );
+
+        $gameHandler->sendToChannel($db, $token, $user->parameters['gameId']);
+    }
+
+}
