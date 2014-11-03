2a5f190205563dcbe88a6bbbb0884a94ea650f3a
King Bartek <bartek@failpoint.pl>
2014-11-03 06:46:42 +0100

    m

--- application/modules/cli/models/Game.php
+++ application/modules/cli/models/Game.php
@@ -13,18 +13,24 @@ class Cli_Model_Game
     private $_ruins = array();
 
     private $_capitals = array();
+    private $_teams = array();
+    private $_playersInGameColors;
 
     private $_begin;
     private $_turnsLimit;
     private $_turnTimeLimit;
     private $_timeLimit;
 
-    private $_units;
-    private $_terrain;
-
     private $_turnHistory;
 
     private $_me;
+
+    private $_fields;
+    private $_units;
+    private $_specialUnits = array();
+    private $_firstUnitId;
+    private $_terrain;
+
 
     public function __construct($gameId, Zend_Db_Adapter_Pdo_Pgsql $db)
     {
@@ -39,16 +45,39 @@ class Cli_Model_Game
         $this->_turnTimeLimit = $game['turnTimeLimit'];
         $this->_timeLimit = $game['timeLimit'];
 
-        $mUnit = new Application_Model_MapUnits($this->_mapId, $db);
-        $this->_units = $mUnit->getUnits();
         $mTerrain = new Application_Model_MapTerrain($this->_mapId, $db);
         $this->_terrain = $mTerrain->getTerrain();
 
         $mTurnHistory = new Application_Model_TurnHistory($this->_id, $db);
         $this->_turnHistory = $mTurnHistory->getTurnHistory();
 
+        $mPlayersInGame = new Application_Model_PlayersInGame($this->_id, $db);
+        $this->_teams = $mPlayersInGame->getTeams();
+        $this->_playersInGameColors = $mPlayersInGame->getAllColors();
+
+        $mChat = new Application_Model_Chat($this->_id, $db);
+        $this->_chatHistory = $mChat->getChatHistory();
+        foreach ($this->_chatHistory as $k => $v) {
+            $this->_chatHistory[$k]['color'] = $this->_playersInGameColors[$v['playerId']];
+        }
+
         $mMapPlayers = new Application_Model_MapPlayers($this->_mapId, $db);
         $this->_capitals = $mMapPlayers->getCapitals();
+
+        $mMapFields = new Application_Model_MapFields($this->_mapId, $db);
+        $this->_fields = $mMapFields->getMapFields();
+
+        $mMapUnits = new Application_Model_MapUnits($this->_mapId, $db);
+        $this->_units = $mMapUnits->getUnits();
+
+        foreach ($this->_units as $unit) {
+            if ($unit['special']) {
+                $this->_specialUnits[] = $unit;
+            }
+        }
+
+        reset($this->_units);
+        $this->_firstUnitId = key($this->_units);
 
         $this->initPlayers($mMapPlayers, $db);
         $this->initRuins($db);
