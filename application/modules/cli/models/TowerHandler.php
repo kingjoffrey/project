aee15519c856de8434c2648fe16bff5e74c6d2d8
King Bartek <bartek@failpoint.pl>
2015-02-18 04:28:40 +0100

    m

--- application/modules/cli/models/TowerHandler.php
+++ application/modules/cli/models/TowerHandler.php
@@ -2,7 +2,6 @@
 
 class Cli_Model_TowerHandler
 {
-
     public function __construct($playerId, Cli_Model_Path $path, Cli_Model_Game $game, Zend_Db_Adapter_Pdo_Pgsql $db, Cli_GameHumansHandler $gameHandler)
     {
         $current = $path->getCurrent();
@@ -11,54 +10,43 @@ class Cli_Model_TowerHandler
             return;
         }
 
-        $surroundings = array();
-
-        foreach ($current as $step) {
-            $surroundings[$step['y'] - 1][$step['x'] - 1] = 1;
-            $surroundings[$step['y'] - 1][$step['x']] = 1;
-            $surroundings[$step['y'] - 1][$step['x'] + 1] = 1;
-            $surroundings[$step['y']][$step['x'] - 1] = 1;
-            $surroundings[$step['y']][$step['x']] = 1;
-            $surroundings[$step['y']][$step['x'] + 1] = 1;
-            $surroundings[$step['y'] + 1][$step['x'] - 1] = 1;
-            $surroundings[$step['y'] + 1][$step['x']] = 1;
-            $surroundings[$step['y'] + 1][$step['x'] + 1] = 1;
-        }
-
         $gameId = $game->getId();
         $fields = $game->getFields();
         $players = $game->getPlayers();
         $playerColor = $game->getPlayerColor($playerId);
-        $playerTeam = $players->getPlayer($playerColor)->getTeam();
+        $player = $players->getPlayer($playerColor);
+        $playerTeam = $player->getTeam();
 
-        foreach ($surroundings as $y => $row) {
-            foreach (array_keys($row) as $x) {
-                if ($towerId = $fields->isTower($x, $y)) {
-                    if ($fields->isArmy($x, $y)) {
-                        continue;
+        foreach ($current as $step) {
+            for ($y = $step['y'] - 1; $y <= $step['y'] + 1; $y++) {
+                for ($x = $step['x'] - 1; $x <= $step['x'] + 1; $x++) {
+                    if ($towerId = $fields->isTower($x, $y)) {
+                        if ($fields->isArmy($x, $y)) {
+                            continue;
+                        }
+
+                        $towerColor = $fields->getTowerColor($x, $y);
+
+                        if ($towerColor == $playerColor) {
+                            continue;
+                        }
+
+                        $oldOwner = $players->getPlayer($towerColor);
+                        if ($playerTeam == $oldOwner->getTeam()) {
+                            continue;
+                        }
+
+                        $player->addTower($towerId, $oldOwner->getTowers()->getTower($towerId), $towerColor, $fields, $gameId, $db);
+                        $oldOwner->removeTower($towerId);
+
+                        $token = array(
+                            'type' => 'tower',
+                            'x' => $x,
+                            'y' => $y,
+                            'color' => $playerColor
+                        );
+                        $gameHandler->sendToChannel($db, $token, $gameId);
                     }
-
-                    $towerColor = $fields->getTowerColor($x, $y);
-
-                    if ($towerColor == $playerColor) {
-                        continue;
-                    }
-
-                    $oldOwner = $players->getPlayer($towerColor);
-                    if ($playerTeam == $oldOwner->getTeam()) {
-                        continue;
-                    }
-
-                    $players->getPlayer($playerColor)->addTower($towerId, $oldOwner->getTowers()->getTower($towerId), $towerColor, $fields, $gameId, $db);
-                    $oldOwner->removeTower($towerId);
-
-                    $token = array(
-                        'type' => 'tower',
-                        'x' => $x,
-                        'y' => $y,
-                        'color' => $playerColor
-                    );
-                    $gameHandler->sendToChannel($db, $token, $gameId);
                 }
             }
         }
