c2d38dddabe9eaffbe901a5ac084540386e9dc47
King Bartek <bartek@failpoint.pl>
2015-03-20 10:29:27 +0100

    m

---
+++ application/modules/admin/controllers/MapfieldsController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Admin_MapfieldsController extends Coret_Controller_Backend
+{
+
+    public function init()
+    {
+        $this->view->title = 'Map fields';
+        parent::init();
+    }
+}
--- application/modules/admin/models/Mapfields.php
+++ application/modules/admin/models/Mapfields.php
@@ -1,29 +1,21 @@
 <?php
 
-class Admin_Model_Mapcastles extends Coret_Model_ParentDb
+class Admin_Model_Mapfields extends Coret_Model_ParentDb
 {
     protected $_name = 'mapfields';
     protected $_primary = 'mapFieldId';
-    protected $_sequence = 'mapcastles_mapCastleId_seq';
+    protected $_sequence = 'mapfields_mapFieldId_seq';
     protected $_columns = array(
-        'mapCastleId' => array('label' => 'Map castle ID', 'type' => 'number'),
-        'castleId' => array('label' => 'Castle ID', 'type' => 'select'),
         'mapId' => array('label' => 'Map ID', 'type' => 'select'),
         'x' => array('label' => 'X', 'type' => 'number'),
         'y' => array('label' => 'Y', 'type' => 'number'),
-        'enclaveNumber' => array('label' => 'Enclave number', 'type' => 'number')
+        'type' => array('label' => 'Type', 'type' => 'varchar')
     );
 
     static public function getMapIdArray()
     {
         $mMap = new Application_Model_Map();
         return $mMap->getAllMapsList();
-    }
-
-    static public function getCastleIdArray()
-    {
-        $mCastle = new Application_Model_Castle();
-        return $mCastle->getAll();
     }
 }
 
