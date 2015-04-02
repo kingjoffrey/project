040148a09bede0bd17ae8a34a0d0098c2235f11d
King Bartek <bartek@failpoint.pl>
2015-04-03 01:59:42 +0200

    m

--- public/js/game/message.js
+++ public/js/game/message.js
@@ -1,91 +1,67 @@
 var Message = new function () {
-    var element = $('#goldBox')
+    var max = false,
+        maxHeight,
+        maxWidth
 
     this.remove = function (id) {
         if (isSet(id)) {
             $('#' + id).fadeOut(200, function () {
-                this.remove();
+                $('#' + id).remove()
             })
         } else {
-            if (notSet($('.message'))) {
-                return;
-            }
-            $('.message').remove();
+            $('.message').remove()
         }
     }
     this.show = function (title, txt) {
         this.remove()
         var id = makeId(10)
-        element().after(
+        $('#goldBox').after(
             $('<div>')
                 .addClass('message box')
+                .attr('id', id)
                 .append($('<h3>').html(title).addClass('msgTitle'))
                 .append($(txt).addClass('overflow'))
-                .attr('id', id)
                 .fadeIn(200)
         )
         this.adjust(id)
         return id
     }
     this.adjust = function (id) {
-        var maxHeight = Three.getHeight() - 140,
-            maxWidth = Three.getWidth() - 480
+        maxHeight = Three.getHeight() - 140
+        maxWidth = Three.getWidth() - 40
 
-        if (isSet(id)) {
-            if (maxHeight < parseInt($('#' + id).css('min-height'))) {
-                maxHeight = parseInt($('#' + id).css('min-height'))
-            }
-            if (maxWidth < parseInt($('#' + id).css('min-width'))) {
-                maxWidth = parseInt($('#' + id).css('min-width'))
-            }
+        if (maxHeight < parseInt($('#' + id).css('min-height'))) {
+            maxHeight = parseInt($('#' + id).css('min-height'))
 
-            $('#' + id).css({
-                'max-width': maxWidth + 'px',
-                'max-height': maxHeight + 'px'
-            })
-
-            var left = Three.getWidth() / 2 - $('#' + id).outerWidth() / 2
-
-            if (left < $('#mapBox').width() + 30) {
-                left = $('#mapBox').width() + 30
-            }
-
-            $('#' + id).css({
-                left: left + 'px'
-            })
-        } else if ($('.message').length) {
-            if (maxHeight < parseInt($('.message').css('min-height'))) {
-                maxHeight = parseInt($('.message').css('min-height'))
-            }
-            if (maxWidth < parseInt($('.message').css('min-width'))) {
-                maxWidth = parseInt($('.message').css('min-width'))
-            }
-
-            $('.message').css({
-                'max-width': maxWidth + 'px',
-                'max-height': maxHeight + 'px'
-            })
-
-            var left = Three.getWidth() / 2 - $('.message').outerWidth() / 2;
-
-            if (left < $('#mapBox').width() + 30) {
-                left = $('#mapBox').width() + 30
-            }
-
-            $('.message').css({
-                left: left + 'px'
-            })
         }
+        if (maxWidth < parseInt($('#' + id).css('min-width'))) {
+            maxWidth = parseInt($('#' + id).css('min-width'))
+        }
+
+        $('#' + id).css({
+            'max-width': maxWidth + 'px',
+            'max-height': maxHeight + 'px'
+        })
+
+        var left = Three.getWidth() / 2 - $('#' + id).outerWidth() / 2
+
+        $('#' + id).css({
+            left: left + 'px'
+        })
     }
     this.setOverflowHeight = function (id) {
-        var minus = 0
-        if (isSet(id)) {
-            var height = $('#' + id).height() - minus;
-            $('#' + id + ' div.overflow').css('height', height + 'px')
+        if ($('#' + id + ' .showCastle').length) {
+            var minus = 0
+        } else if ($('#' + id).height() == maxHeight) {
+            var minus = 65
         } else {
-            var height = $('.message').height() - minus;
-            $('.message' + ' div.overflow').css('height', height + 'px')
+            var minus = 20
         }
+
+        var height = $('#' + id).height() - minus;
+
+        $('#' + id + ' div.overflow').css('height', height + 'px')
+
         if (Me.isSelected()) {
             Me.setIsSelected(0)
         }
