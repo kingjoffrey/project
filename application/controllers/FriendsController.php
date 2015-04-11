91848de6e3f758aa2abbceabcd9f81b086644664
King Bartek <bartek@failpoint.pl>
2015-04-11 17:06:33 +0200

    m

---
+++ application/controllers/FriendsController.php
@@ -0,0 +1,11 @@
+<?php
+
+class FriendsController extends Game_Controller_Gui
+{
+    public function indexAction()
+    {
+        $mFriend = new Application_Model_Friends();
+        $this->view->friends = $mFriend->getFriends($this->_request->getParam('page'), Zend_Auth::getInstance()->getIdentity()->playerId);
+    }
+}
+
---
+++ application/models/Friends.php
@@ -0,0 +1,43 @@
+<?php
+
+class Application_Model_Friends extends Coret_Db_Table_Abstract
+{
+    protected $_name = 'friends';
+
+    public function __construct(Zend_Db_Adapter_Pdo_Pgsql $db = null)
+    {
+        if ($db) {
+            $this->_db = $db;
+        } else {
+            parent::__construct();
+        }
+    }
+
+    public function create($playerId, $friendId)
+    {
+        $this->insert(array('playerId' => $playerId, 'friendId' => $friendId));
+    }
+
+    public function remove($playerId, $friendId)
+    {
+        $where = array(
+            $this->_db->quoteInto($this->_db->quoteIdentifier('playerId') . ' = ?', $playerId),
+            $this->_db->quoteInto($this->_db->quoteIdentifier('friendId') . ' = ?', $friendId)
+        );
+        $this->delete($where);
+    }
+
+    public function getFriends($pageNumber, $playerId)
+    {
+        $select = $this->_db->select()
+            ->from($this->_name)
+            ->where($this->_db->quoteIdentifier('playerId') . ' = ?', $playerId);
+
+        $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_DbSelect($select));
+        $paginator->setCurrentPageNumber($pageNumber);
+        $paginator->setItemCountPerPage(20);
+
+        return $paginator;
+    }
+}
+
