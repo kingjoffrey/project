f3356ce0ace5e9d3945a61b9e7ed4907e0c9a89d
King Bartek <bartek@failpoint.pl>
2015-12-13 07:50:00 +0100

    m

--- public/js/test.js
+++ public/js/test.js
@@ -6,7 +6,7 @@ var Test = new function () {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true
 
-    camera.position.set(-5, 5, 5);
+    camera.position.set(-150, 150, 150);
     camera.rotation.order = 'YXZ';
     camera.rotation.y = -Math.PI / 4;
     camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
@@ -33,8 +33,8 @@ var Test = new function () {
 var CreateSimpleMesh = new function () {
     var xy = [],
         uv = [],
-        maxX = 2,
-        maxY = 2,
+        maxX = 20,
+        maxY = 20,
         river = [[0, 5], [0, 4], [1, 3], [2, 2], [3, 2], [4, 1], [5, 1], [6, 0]],
         grassGeometry = new THREE.BufferGeometry(),
         grassVertexPositions = [],
@@ -43,16 +43,28 @@ var CreateSimpleMesh = new function () {
             [1, 0],
             [0, 1],
             [1, 1]
-        ],
-        image = new Image()
+        ]
 
     this.init = function () {
+        for (var u = 0; u < maxX; u++) {
+            uv[u] = []
+            for (var v = 0; v < maxY; v++) {
+                uv[u][v] = []
+                uv[u][v][0] = [u / maxX, v / maxY]
+                uv[u][v][1] = [(u + 1) / maxX, v / maxY]
+                uv[u][v][2] = [u / maxX, (v + 1) / maxY]
+                uv[u][v][3] = [(u + 1) / maxX, (v + 1) / maxY]
+            }
+        }
+        console.log(imageArray)
+        console.log(uv)
+
         for (var i = 0; i < maxX; i++) {
             for (var j = 0; j < maxY; j++) {
                 xy.push([i, j])
             }
         }
-        //console.log(xy)
+
         for (var i = 0; i < xy.length; i++) {
             grassVertexPositions.push([xy[i][0], xy[i][1], 0])
             grassVertexPositions.push([xy[i][0] + 1, xy[i][1], 0])
@@ -62,73 +74,39 @@ var CreateSimpleMesh = new function () {
             grassVertexPositions.push([xy[i][0], xy[i][1] + 1, 0])
             grassVertexPositions.push([xy[i][0] + 1, xy[i][1], 0])
         }
-        //console.log(grassVertexPositions)
-        //for (var i = 0; i < grassVertexPositions.length; i++) {
-        //    for (var j = 0; j < river.length; j++) {
-        //        if (river[j][0] == grassVertexPositions[i][0] && river[j][1] == grassVertexPositions[i][1]) {
-        //            grassVertexPositions[i][2] = -0.5
-        //        }
-        //    }
-        //}
 
         var grassVertices = new Float32Array(grassVertexPositions.length * 3),
             normals = new Float32Array(grassVertexPositions.length * 3),
             colors = new Float32Array(grassVertexPositions.length * 3),
             uvs = new Float32Array(grassVertexPositions.length * 2)
 
-        for (var k = 0; k < maxX * maxY; k++) {
-            uv[k] = []
-            for (var i = 0; i <= maxY; i++) {
-                for (var j = 0; j <= maxX; j++) {
-                    uv[k].push([j / maxX, i / maxY])
-                }
-            }
-        }
-        console.log(imageArray)
-        console.log(uv)
-        var j = 0,
-            k = 0
-
         for (var i = 0; i < grassVertexPositions.length; i++) {
             var index = 3 * i
-            if (j > 5) {
-                j = 0
-                k += 12
-            }
             grassVertices[index + 0] = grassVertexPositions[i][0]
             grassVertices[index + 1] = grassVertexPositions[i][1]
             grassVertices[index + 2] = grassVertexPositions[i][2]
-            switch (j) {
-                // first triangle
-                case 0:
-                    uvs[0 + k] = imageArray[0][0] / maxX
-                    uvs[1 + k] = imageArray[0][1] / maxY
-                    break
-                case 1:
-                    uvs[2 + k] = imageArray[1][0] / maxX
-                    uvs[3 + k] = imageArray[1][1] / maxY
-                    break
-                case 2:
-                    uvs[4 + k] = imageArray[2][0] / maxX
-                    uvs[5 + k] = imageArray[2][1] / maxY
-                    break
-                // second triangle
-                case 3:
-                    uvs[6 + k] = imageArray[3][0] / maxX
-                    uvs[7 + k] = imageArray[3][1] / maxY
-                    break
-                case 4:
-                    uvs[8 + k] = imageArray[2][0] / maxX
-                    uvs[9 + k] = imageArray[2][1] / maxY
-                    break
-                case 5:
-                    uvs[10 + k] = imageArray[1][0] / maxX
-                    uvs[11 + k] = imageArray[1][1] / maxY
-                    break
-            }
-            j++
         }
 
+        var k = 0
+        for (var u = 0; u < maxX; u++) {
+            for (var v = 0; v < maxY; v++) {
+                // first triangle
+                uvs[0 + k] = uv[u][v][0][0]
+                uvs[1 + k] = uv[u][v][0][1]
+                uvs[2 + k] = uv[u][v][1][0]
+                uvs[3 + k] = uv[u][v][1][1]
+                uvs[4 + k] = uv[u][v][2][0]
+                uvs[5 + k] = uv[u][v][2][1]
+                // second triangle
+                uvs[6 + k] = uv[u][v][3][0]
+                uvs[7 + k] = uv[u][v][3][1]
+                uvs[8 + k] = uv[u][v][2][0]
+                uvs[9 + k] = uv[u][v][2][1]
+                uvs[10 + k] = uv[u][v][1][0]
+                uvs[11 + k] = uv[u][v][1][1]
+                k += 12
+            }
+        }
         console.log(uvs)
 
         grassGeometry.addAttribute('position', new THREE.BufferAttribute(grassVertices, 3))
@@ -144,7 +122,7 @@ var CreateSimpleMesh = new function () {
             var grassMaterial = new THREE.MeshLambertMaterial({map: texture}),
                 grassMesh = new THREE.Mesh(grassGeometry, grassMaterial)
 
-            grassMesh.rotation.x = -Math.PI / 2;
+            //grassMesh.rotation.x = -Math.PI / 2;
             Test.getScene().add(grassMesh)
 
             var helper = new THREE.WireframeHelper(grassMesh, 0xff00ff);
