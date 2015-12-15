445f648c503bed08ab62288b3a962c6ea255143e
King Bartek <bartek@failpoint.pl>
2015-12-15 09:24:53 +0100

    m

--- application/controllers/EditorController.php
+++ application/controllers/EditorController.php
@@ -26,23 +26,17 @@ class EditorController extends Game_Controller_Gui
         $this->view->headLink()->appendStylesheet($this->view->baseUrl() . '/css/editor.css?v=' . Zend_Registry::get('config')->version);
 
 //        $this->view->headScript()->appendFile('/js/kinetic-v4.7.4.min.js');
-        $this->view->headScript()->appendFile('http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v5.0.1.min.js');
-        $this->view->Websocket();
+        $this->view->headScript()->appendFile('/js/kinetic-v5.1.0.min.js');
         $this->view->headScript()->appendFile('/js/editor/castle.js?v=' . Zend_Registry::get('config')->version);
         $this->view->headScript()->appendFile('/js/editor/diamondsquare.js?v=' . Zend_Registry::get('config')->version);
         $this->view->headScript()->appendFile('/js/editor/editor.js?v=' . Zend_Registry::get('config')->version);
         $this->view->headScript()->appendFile('/js/editor/gui.js?v=' . Zend_Registry::get('config')->version);
         $this->view->headScript()->appendFile('/js/editor/init.js?v=' . Zend_Registry::get('config')->version);
-        $this->view->headScript()->appendFile('/js/editor/websocket.js?v=' . Zend_Registry::get('config')->version);
 
         $mapId = $this->_request->getParam('mapId');
 
         $mMap = new Application_Model_Map($mapId);
         $this->view->map = $mMap->getMap(Zend_Auth::getInstance()->getIdentity()->playerId);
-
-        $mWebSocket = new Application_Model_Websocket(Zend_Auth::getInstance()->getIdentity()->playerId);
-        $this->view->websocket = $mWebSocket->connect('editor');
-        $this->view->playerId = Zend_Auth::getInstance()->getIdentity()->playerId;
 
         $mMapCastles = new Application_Model_MapCastles($mapId);
         $this->view->mapCastles = $mMapCastles->getMapCastles();
