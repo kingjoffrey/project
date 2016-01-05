7f74d5b3937d381c4e3eb2cb25e9b7fbe80cc13e
King Bartek <bartek@failpoint.pl>
2016-01-05 01:05:44 +0100

    m

---
+++ public/js/editor/websocket.js
@@ -0,0 +1,52 @@
+var WebSocketEditor = new function () {
+    var closed = true,
+        ws,
+        onMessage = function (r) {
+            console.log(r)
+        },
+        open = function () {
+            if (closed) {
+                console.log(translations.sorryServerIsDisconnected)
+                return;
+            }
+
+            var token = {
+                type: 'open',
+                playerId: id,
+                name: playerName,
+                langId: langId,
+                accessKey: accessKey
+            }
+
+            ws.send(JSON.stringify(token));
+        }
+    this.save = function () {
+        var token = {
+            type: 'save',
+            mapId: mapId,
+            map: MapGenerator.getImage()
+        }
+
+        ws.send(JSON.stringify(token))
+    }
+
+    this.init = function () {
+        ws = new WebSocket(wsURL + '/editor')
+
+        ws.onopen = function () {
+            closed = false
+            open()
+
+            if (!MapGenerator.getInit()) {
+                MapGenerator.init()
+            }
+        }
+        ws.onmessage = function (e) {
+            onMessage($.parseJSON(e.data))
+        }
+        ws.onclose = function () {
+            closed = true;
+            setTimeout('WebSocketEditor.init()', 1000)
+        }
+    }
+}
