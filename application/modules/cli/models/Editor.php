7af13f66d29c7bb2f37a959a3caf52a5a0ae2e74
King Bartek <bartek@failpoint.pl>
2016-01-13 12:33:26 +0100

    m

--- application/modules/cli/models/Editor.php
+++ application/modules/cli/models/Editor.php
@@ -2,23 +2,56 @@
 
 class Cli_Model_Editor
 {
-    private $_id;
-    private $_fields;
-
+    private $_mapId;
+    private $_Fields;
+    private $_Players;
+    private $_Ruins;
 
     public function __construct($mapId, Zend_Db_Adapter_Pdo_Pgsql $db)
     {
-        $this->_id = $mapId;
+        $this->_mapId = $mapId;
 
-        $mMapFields = new Application_Model_MapFields($this->_id, $db);
-        $this->_fields = new Cli_Model_Fields($mMapFields->getMapFields());
+        $mMapFields = new Application_Model_MapFields($this->_mapId, $db);
+        $this->_Fields = new Cli_Model_Fields($mMapFields->getMapFields());
+
+        $mMapPlayers = new Application_Model_MapPlayers($this->_mapId, $db);
+
+        $this->_Players = new Cli_Model_Players();
+        $this->_Ruins = new Cli_Model_Ruins();
+
+        $players = array();
+        $this->initPlayers($mMapPlayers, $players, $db);
+    }
+
+    private function initPlayers(Application_Model_MapPlayers $mMapPlayers, $players, Zend_Db_Adapter_Pdo_Pgsql $db)
+    {
+        $mMapCastles = new Application_Model_MapCastles($this->_mapId, $db);
+        $mapCastles = $mMapCastles->getMapCastles();
+        $mMapTowers = new Application_Model_MapTowers($this->_mapId, $db);
+        $mapTowers = $mMapTowers->getMapTowers();
+//        $mTowersInGame = new Application_Model_TowersInGame($this->_id, $db);
+//        $playersTowers = $mTowersInGame->getTowers();
+
+        $playersTowers = array();
+
+        foreach ($this->_playersColors as $playerId => $color) {
+            $player = new Cli_Model_Player($players[$playerId], $this->_id, $mapCastles, $mapTowers, $playersTowers, $mMapPlayers, $db);
+            $this->_Players->addPlayer($color, $player);
+            if (!$player->getComputer()) {
+                $this->updateOnline($color, 0);
+            }
+        }
+        $player = new Cli_Model_NeutralPlayer($this, $mapCastles, $mapTowers, $playersTowers, $db);
+        $this->_Players->addPlayer('neutral', $player);
+
+        $this->_Players->initFields($this->_Fields);
     }
 
     public function toArray()
     {
         return array(
             'id' => $this->_id,
-            'fields' => $this->_fields->toArray()
+            'fields' => $this->_Fields->toArray()
         );
     }
 
---
+++ application/modules/cli/models/EditorPlayer.php
@@ -0,0 +1,180 @@
+<?php
+
+class Cli_Model_EditorPlayer extends Cli_Model_DefaultPlayer
+{
+    private $_computer;
+
+    private $_gold;
+
+    private $_miniMapColor;
+    private $_textColor;
+
+    public function __construct($player, $mapCastles, $mapTowers, $playersTowers, Application_Model_MapPlayers $mMapPlayers, Zend_Db_Adapter_Pdo_Pgsql $db)
+    {
+        $this->_id = $player['playerId'];
+
+        $this->_computer = $player['computer'];
+        $this->_gold = $player['gold'];
+        $this->_miniMapColor = $player['minimapColor'];
+        $this->_backgroundColor = $player['backgroundColor'];
+        $this->_textColor = $player['textColor'];
+        $this->_longName = $player['longName'];
+
+        $this->_team = $mMapPlayers->getColorByMapPlayerId($player['team']);
+        $this->_color = $player['color'];
+
+        $this->_armies = new Cli_Model_Armies();
+        $this->_castles = new Cli_Model_Castles();
+        $this->_towers = new Cli_Model_Towers();
+
+        $this->initArmies($gameId, $db);
+        $this->initCastles($gameId, $mapCastles, $db);
+        $this->initTowers($mapTowers, $playersTowers);
+        $this->initBattleSequence($gameId, $db);
+    }
+
+    private function initBattleSequence($gameId, Zend_Db_Adapter_Pdo_Pgsql $db)
+    {
+        $mBattleSequence = new Application_Model_BattleSequence($gameId, $db);
+        $battleSequence = $mBattleSequence->get($this->_id);
+        if (empty($battleSequence['attack'])) {
+            $mBattleSequence->initiate($this->_id, Zend_Registry::get('units'));
+            $battleSequence = $mBattleSequence->get($this->_id);
+        }
+        $this->setAttackBattleSequence($battleSequence['attack']);
+        $this->setDefenceBattleSequence($battleSequence['defence']);
+    }
+
+    public function setAttackBattleSequence($sequence)
+    {
+        $this->_attackSequence = $sequence;
+    }
+
+    public function setDefenceBattleSequence($sequence)
+    {
+        $this->_defenceSequence = $sequence;
+    }
+
+    private function initArmies($gameId, Zend_Db_Adapter_Pdo_Pgsql $db)
+    {
+        $mArmy = new Application_Model_Army($gameId, $db);
+        $mSoldier = new Application_Model_UnitsInGame($gameId, $db);
+        $mHeroesInGame = new Application_Model_HeroesInGame($gameId, $db);
+
+        foreach ($mArmy->getPlayerArmies($this->_id) as $a) {
+            $this->_armies->addArmy($a['armyId'], new Cli_Model_Army($a, $this->_color));
+            $army = $this->_armies->getArmy($a['armyId']);
+            $army->initHeroes($mHeroesInGame->getForMove($a['armyId']));
+            $army->initSoldiers($mSoldier->getForMove($a['armyId']));
+        }
+    }
+
+    private function initCastles($gameId, $mapCastles, Zend_Db_Adapter_Pdo_Pgsql $db)
+    {
+        $mCastlesInGame = new Application_Model_CastlesInGame($gameId, $db);
+        $mCastleProduction = new Application_Model_CastleProduction($db);
+        foreach ($mCastlesInGame->getPlayerCastles($this->_id) as $castleId => $c) {
+            $this->_castles->addCastle($castleId, new Cli_Model_Castle($c, $mapCastles[$castleId]));
+            $castle = $this->_castles->getCastle($castleId);
+            $castle->initProduction($mCastleProduction->getCastleProduction($castleId));
+        }
+    }
+
+    private function initTowers($mapTowers, $playersTowers)
+    {
+        foreach ($playersTowers as $towerId => $playerId) {
+            if ($playerId != $this->_id) {
+                continue;
+            }
+            $tower = $mapTowers[$towerId];
+            $tower['towerId'] = $towerId;
+            $this->_towers->add($towerId, new Cli_Model_Tower($tower));
+        }
+    }
+
+    public function toArray()
+    {
+        return array(
+            'turnActive' => $this->_turnActive,
+            'computer' => $this->_computer,
+            'lost' => $this->_lost,
+            'miniMapColor' => $this->_miniMapColor,
+            'backgroundColor' => $this->_backgroundColor,
+            'textColor' => $this->_textColor,
+            'longName' => $this->_longName,
+            'team' => $this->_team,
+            'armies' => $this->_armies->toArray(),
+            'castles' => $this->_castles->toArray(),
+            'towers' => $this->_towers->toArray()
+        );
+    }
+
+    public function setLost($gameId, $db)
+    {
+        $mPlayersInGame = new Application_Model_PlayersInGame($gameId, $db);
+        $mPlayersInGame->setPlayerLostGame($this->_id);
+        $this->_lost = true;
+    }
+
+    public function getGold()
+    {
+        return $this->_gold;
+    }
+
+    public function subtractGold($gold)
+    {
+        $this->_gold -= $gold;
+    }
+
+    public function addGold($gold)
+    {
+        $this->_gold += $gold;
+    }
+
+    public function setTurnActive($turnActive)
+    {
+        $this->_turnActive = $turnActive;
+    }
+
+    public function addTower($towerId, Cli_Model_Tower $tower, $oldColor, Cli_Model_Fields $fields, $gameId, $db)
+    {
+        $fields->getField($tower->getX(), $tower->getY())->setTowerColor($this->_color);
+        $this->_towers->add($towerId, $tower, $oldColor, $this->_id, $gameId, $db);
+    }
+
+    public function getComputer()
+    {
+        return $this->_computer;
+    }
+
+    public function getTurnActive()
+    {
+        return $this->_turnActive;
+    }
+
+    public function getAttackSequence()
+    {
+        return $this->_attackSequence;
+    }
+
+    public function getDefenceSequence()
+    {
+        return $this->_defenceSequence;
+    }
+
+    public function noArmiesAndCastles()
+    {
+        return $this->_castles->noCastlesExists() && $this->_armies->noArmiesExists();
+    }
+
+    public function armiesOrCastlesExists()
+    {
+        return $this->_armies->exists() || $this->_castles->castlesExists();
+    }
+
+    public function saveGold($gameId, $db)
+    {
+        $mPlayersInGame = new Application_Model_PlayersInGame($gameId, $db);
+        $mPlayersInGame->updatePlayerGold($this->_id, $this->_gold);
+    }
+}
