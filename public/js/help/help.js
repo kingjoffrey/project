f08ab58c41bb3b9aa53ebdc09619094ecf71eca8
King Bartek <bartek@failpoint.pl>
2016-03-25 23:43:16 +0100

    m

--- public/js/help/help.js
+++ public/js/help/help.js
@@ -1,6 +1,34 @@
 "use strict"
 var Help = new function () {
+    var help,
+        text
+
+    this.click = function (id) {
+        this.fillText(help[id])
+    }
+    this.fillText = function (action) {
+        text.html('')
+        for (var i in action) {
+            text
+                .append($('<h5>').html(action[i].title))
+                .append($('<p>').html(Help.nl2br(action[i].content)))
+        }
+
+    }
+    this.set = function (r) {
+        help = r
+    }
+    this.nl2br = function (str, is_xhtml) {
+        var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+        return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    }
     this.init = function () {
+        $('#helpMenu div').click(function () {
+            Help.click($(this).attr('id'))
+        })
+
+        text = $('#text')
+
         WebSocketHelp.init()
     }
 }
--- public/js/help/websocketMessage.js
+++ public/js/help/websocketMessage.js
@@ -3,22 +3,11 @@ var WebSocketMessage = new function () {
         console.log(r)
         switch (r.type) {
             case 'open':
-                console.log($('.table').length)
-                var table = $('.table')
-                for (var i in r.game) {
-                    table
-                        .append($('<h5>').html(r.game[i].title))
-                        .append($('<p>').html(nl2br(r.game[i].content)))
-
-                }
+                Help.set(r)
+                Help.fillText(r.game)
                 break
             default:
                 console.log(r)
         }
     }
-}
-
-function nl2br(str, is_xhtml) {
-    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
-    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 }
