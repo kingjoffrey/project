d030d4b861e6a1e3e49aaa7adf4241de189b423f
King Bartek <bartek@failpoint.pl>
2018-08-19 17:42:48 +0200

    m

--- public/js/main/index.js
+++ public/js/main/index.js
@@ -2,30 +2,25 @@
 var IndexController = new function () {
     this.index = function () {
         if (WebSocketGame.isOpen()) {
-            console.log('a1')
             WebSocketGame.close()
             WebSocketExecGame.close()
             GameRenderer.stop()
             Game.resetLoading()
         }
         if (WebSocketTutorial.isOpen()) {
-            console.log('a2')
             WebSocketTutorial.close()
             WebSocketExecTutorial.close()
             GameRenderer.stop()
             Game.resetLoading()
         }
         if (WebSocketEditor.isOpen()) {
-            console.log('a3')
             WebSocketEditor.close()
             GameRenderer.stop()
         }
         if (WebSocketNew.isOpen()) {
-            console.log('a4')
             WebSocketNew.close()
         }
         if (HelpRenderer.isRunning()) {
-            console.log('a5')
             HelpRenderer.stop()
         }
 
